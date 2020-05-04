04f2708284ba95da9b726a363bcd27b46f77e159
Brian Bockelman <bbockelm@cse.unl.edu>
2020-05-04 15:48:34 -0500

    Fix startup scripts.

--- build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
+++ build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-exec cp /etc/condor/tokens-orig.d/* /etc/condor/tokens.d/
+if [ $(ls -A /etc/condor/tokens-orig.d/* 2>/dev/null) ];
+  cp /etc/condor/tokens-orig.d/* /etc/condor/tokens.d/
+fi
