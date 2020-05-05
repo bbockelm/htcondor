fa1e15b166028af8eafdec2dfa9a24caf5908d4e
Brian Bockelman <bbockelm@cse.unl.edu>
2020-05-05 14:38:19 -0500

    Include a negotiator password for token generation.

--- build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
+++ build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
@@ -9,3 +9,13 @@ if [ $(ls -A /etc/condor/tokens-orig.d/* 2>/dev/null) ]; then
   chmod 0600 /etc/condor/tokens-tmp.d/*
   mv /etc/condor/tokens-tmp.d/* /etc/condor/tokens.d
 fi
+
+if [ $(ls -A /etc/condor/passwords-orig.d/* 2>/dev/null) ]; then
+  mkdir -p /etc/condor/passwords-tmp.d
+  cp /etc/condor/passwords-orig.d/* /etc/condor/passwords-tmp.d/
+  chown -R root: /etc/condor/passwords.d
+  chown -R root: /etc/condor/passwords-tmp.d
+  chmod 0700 /etc/condor/passwords.d
+  chmod 0600 /etc/condor/passwords-tmp.d/*
+  mv /etc/condor/passwords-tmp.d/* /etc/condor/passwords.d
+fi
