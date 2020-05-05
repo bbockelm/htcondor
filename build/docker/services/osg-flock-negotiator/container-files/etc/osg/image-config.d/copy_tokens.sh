bddb6aefb4ed3d7890277b349b1bacd4ca1fe769
Brian Bockelman <bbockelm@cse.unl.edu>
2020-05-04 22:26:28 -0500

    Fix file ownerships.

--- build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
+++ build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
@@ -3,8 +3,9 @@
 if [ $(ls -A /etc/condor/tokens-orig.d/* 2>/dev/null) ]; then
   mkdir -p /etc/condor/tokens-tmp.d
   cp /etc/condor/tokens-orig.d/* /etc/condor/tokens-tmp.d/
+  chown -R condor: /etc/condor/tokens.d
   chown -R condor: /etc/condor/tokens-tmp.d
-  chmod 0700 /etc/condor/tokens-tmp.d
+  chmod 0700 /etc/condor/tokens.d
   chmod 0600 /etc/condor/tokens-tmp.d/*
-  mv /etc/condor/tokens-tmp.d /etc/condor/tokens.d
+  mv /etc/condor/tokens-tmp.d/* /etc/condor/tokens.d
 fi
