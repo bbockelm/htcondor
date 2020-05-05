4d6064953686d588ff616963c23b731509a2e213
Brian Bockelman <bbockelm@cse.unl.edu>
2020-05-04 21:47:28 -0500

    Fix owner and mode of token files made by cronjob.

--- build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
+++ build/docker/services/osg-flock-negotiator/container-files/etc/osg/image-config.d/copy_tokens.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-if [ $(ls -A /etc/condor/tokens-orig.d/* 2>/dev/null) ];
-  cp /etc/condor/tokens-orig.d/* /etc/condor/tokens.d/
+if [ $(ls -A /etc/condor/tokens-orig.d/* 2>/dev/null) ]; then
+  mkdir -p /etc/condor/tokens-tmp.d
+  cp /etc/condor/tokens-orig.d/* /etc/condor/tokens-tmp.d/
+  chown -R condor: /etc/condor/tokens-tmp.d
+  chmod 0700 /etc/condor/tokens-tmp.d
+  chmod 0600 /etc/condor/tokens-tmp.d/*
+  mv /etc/condor/tokens-tmp.d /etc/condor/tokens.d
 fi
